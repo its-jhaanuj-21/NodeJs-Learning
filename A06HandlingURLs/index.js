dbe77172d379df1d13f80a69d5e9bf5efeaad491
Anuj Jha <anujjha2106@gmail.com>
2024-06-10 13:21:33 +0530

    url handing

--- A06HandlingURLs/index.js
+++ A06HandlingURLs/index.js
@@ -1,5 +1,6 @@
 const http = require("http");
 const fs = require("fs");
+const url = require("url")
 
 const myNewServer = http.createServer((req , res)=>{
     console.log("New Request Recorded"); 
@@ -7,13 +8,22 @@ const myNewServer = http.createServer((req , res)=>{
     if(req.url === "/favicon.ico") return res.end();
 
     const log = `${Date.now()} : ${req.url} : New Request Received\n`;
+
+    const myUrl = url.parse(req.url, true);
+    console.log(myUrl);
+
     fs.appendFile("log.txt", log, (err, data)=>{
-        switch(req.url){
+        switch(myUrl.pathname){
             case "/":
                 res.end("HomePage");
                 break;
             case "/about":
-                res.end("I'm Anuj Kumar Jha!!");
+                const username = myUrl.query.fullName
+                res.end(`Hii, ${username}`);
+                break;
+            case "/search":
+                const search = myUrl.query.search_query;
+                res.end("Here are your result for "+ search);
                 break;
             default:
                 res.end("404 Not Found!");
